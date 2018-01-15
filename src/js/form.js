7b582d342d4118a0a448c3231f30163d7dcf935a
Manuel <viralk@users.noreply.github.com>
2018-01-15 16:19:58 +0100

    Update form.js

--- src/js/form.js
+++ src/js/form.js
@@ -1,22 +1,50 @@
-$input = document.querySelectorAll('.input-group input');
+        function getInput() {
+          var $input = document.querySelectorAll('.input-group input');
+          return $input;
+        }
+        
 
-for (var i = 0; i < $input.length; i++) {
-  $input[i].addEventListener('keyup', function (e) {
 
-    $this = this;
+        $input = getInput();
+        for (var i = 0; i < $input.length; i++) {
 
-    $this.style.padding = '20px 10px 4px';
-    var el = $this.nextElementSibling;
-    el.style.opacity = '1';
+          $input[i].addEventListener('keyup', function(e) {
 
-    if (e.keyCode == 8 || e.keyCode == 9) {
-      if (!($this.value)) {
-        $this.style.padding = '12px 10px';
-        el.style.opacity = '0';
-      }
-    }
+            modifyStyleInput(this);
 
-  });
-}
+            if (e.keyCode == 8 || e.keyCode == 9 || e.keyCode == 46) {
+              if (!(this.value)) {
+                resetStyleInput(this);
+              }
+            }
 
-// controlla keyup che attivano l'animazione ed escludili
+          });   
+
+          $input[i].addEventListener('cut', function() {
+            resetStyleInput(this);
+          });
+
+        }
+
+
+        function modifyStyleInput(input) {
+          input.style.padding = '20px 10px 4px';
+          input.nextElementSibling.style.opacity = '1';
+        }
+
+        function resetStyleInput(input) {
+          input.style.padding = '12px 10px';
+          input.nextElementSibling.style.opacity = '0';
+        }
+
+        function clearInput(input) {
+          input.value = "";
+          resetStyleInput(input);
+        }
+
+        function clearAll() {
+          $input = getInput();
+          for (var i = 0; i < $input.length; i++) {
+            clearInput($input[i]);
+          }
+        }
