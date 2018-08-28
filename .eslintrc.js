3f965edfcb897541b99491da9977f1534eddabb4
Viralk <manuel.coiai@gmail.com>
2018-08-28 16:00:34 +0200

    post update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,18 @@
 module.exports = {
-    "extends": "airbnb-base",
-      "env": {
-          "browser": true,
-      },
+  "extends": "airbnb-base",
+  "env": {
+      "browser": true,
+      "jquery": true
+  },
+  // Defines global variables and functions used in the app.
+  // True are write-access variables, false are read-only
+  // "globals": {
+  //     "Cookies": true
+  // },
+  "rules": {
+      "no-param-reassign": 0,
+      "max-len": 0,
+      "wrap-iife": 0,
+      "func-names": 0,
+  }
 };
