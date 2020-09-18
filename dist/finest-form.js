cdec8635449c6031a77c87a6e94055630e96b1df
Viralk <manuel.coiai@gmail.com>
2020-09-18 18:59:34 +0200

    Small fix

--- dist/finest-form.js
+++ dist/finest-form.js
@@ -14,16 +14,6 @@ function removeClass(a, b) {
 
 (function () {
 
-  // Add class active to all inputs with a starting value
-  // document.addEventListener('DOMContentLoaded', function () {
-  //   document.querySelectorAll('.input-group input').forEach(function (el) {
-  //     if (el && el.value) {
-  //       addClass(el, 'active');
-  //       addClass(el.nextElementSibling, 'active');
-  //     }
-  //   });
-  // });
-
   document.addEventListener('DOMContentLoaded', function () {
     var el = document.querySelectorAll('.input-group input');
     for (var i = 0; i < el.length; i++) {
@@ -53,7 +43,7 @@ function removeClass(a, b) {
   // Add class active to all inputs that got a value after a state change of
   // the input without focusing on it, such as browser autocomplete
   document.addEventListener('change', function (e) {
-    if (e.target.parentNode.className == 'input-group' && e.target.value) {
+    if (e.target.parentElement.classList.contains('input-group') && e.target.value) {
       addClass(e.target, 'active');
       addClass(e.target.nextElementSibling, 'active');
     }
@@ -61,7 +51,7 @@ function removeClass(a, b) {
 
   // Add class active to the focused input
   document.addEventListener('focusin', function (e) {
-    if (e.target.parentNode.className == 'input-group') {
+    if (e.target.parentElement.classList.contains('input-group')) {
       addClass(e.target, 'active');
       addClass(e.target.nextElementSibling, 'active');
     }
@@ -69,7 +59,7 @@ function removeClass(a, b) {
 
   // If there is no value i remove the active class from the input
   document.addEventListener('focusout', function (e) {
-    if (e.target.parentNode.className == 'input-group' && !e.target.value) {
+    if (e.target.parentElement.classList.contains('input-group') && !e.target.value) {
       removeClass(e.target, 'active');
       removeClass(e.target.nextElementSibling, 'active');
     }
